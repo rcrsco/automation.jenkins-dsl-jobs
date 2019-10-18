dbf2a8d34bcecd74b53c54d6f1b205061955bd8d
rcrsco <56722396+rcrsco@users.noreply.github.com>
2019-10-18 14:25:46 +0100

    Create motherseed_job.groovy

---
+++ motherseed_job.groovy
@@ -0,0 +1,7 @@
+environments {
+    aws {
+        sit {
+            reposToAutomate = [
+                [projectName: "automation.jenkins-dsl-jobs", url: "https://git-internal/ccdb-jenkins-automation"],
+            ]
+        }
